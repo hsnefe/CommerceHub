e24b01d9be7e7e9b90ee931f577d717e4faf69be
hsnefe <119686500+hsnefe@users.noreply.github.com>
2026-08-08 00:56:56 +0300

    Add Resilience4j dependencies and HTTP client timeouts

---
+++ services/order-service/src/main/java/com/commercehub/order/config/HttpClientProperties.java
@@ -0,0 +1,18 @@
+package com.commercehub.order.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "commercehub.http-client")
+public record HttpClientProperties(
+        int connectTimeoutMs,
+        int readTimeoutMs
+) {
+    public HttpClientProperties {
+        if (connectTimeoutMs <= 0) {
+            connectTimeoutMs = 500;
+        }
+        if (readTimeoutMs <= 0) {
+            readTimeoutMs = 2000;
+        }
+    }
+}
--- services/order-service/src/main/java/com/commercehub/order/config/RestClientConfig.java
+++ services/order-service/src/main/java/com/commercehub/order/config/RestClientConfig.java
@@ -2,13 +2,17 @@ package com.commercehub.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(RestClient.Builder builder) {
-        return builder.build();
+    public RestClient restClient(RestClient.Builder builder, HttpClientProperties properties) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(properties.connectTimeoutMs());
+        requestFactory.setReadTimeout(properties.readTimeoutMs());
+        return builder.requestFactory(requestFactory).build();
     }
 }
